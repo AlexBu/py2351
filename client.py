a0ca85822653440fec4c15c53ab87eb223c4773d
alexbu <bkpfx000@gmail.com>
2015-03-15 22:43:05 +0800

    update code;add two capture dumps;add a 010 template

--- client.py
+++ client.py
@@ -17,35 +17,39 @@ def login(socket, username, password):
     loginip = '192.168.1.107  '
     loginversion = '3911010'
     loginsubversion = '000000'
-    loginrespondstr = b'\x00\x14\x01\x00\x03\x00\x00\x00\x39\xd1\x0c\x00\x00\x8c\x8c\x29\x00\x00\xa9\xa9'
     loginstruct = struct.pack("BB16s32s12s15s8s77s", loginhead,
                               loginlength, username, password,
                               loginmac, loginip, loginversion,
                               loginsubversion)
-    #for traits in loginstruct:
-    #    print '%x' %ord(traits)
     
     sendcount = socket.send(loginstruct)
-    #print "send bytes ", sendcount
     data = socket.recv(1024)
-    #print ('received: ', data)
-    #print ('expected: ', loginrespondstr)
-    if cmp(loginrespondstr, data) == 0:
+    
+    validlogin(data)
+
+def validlogin(data):
+    if ((ord(data[1]) == len(data)) and (ord(data[2]) == 1) ):
         print 'login success!'
     else:
-        print 'login failed!'
+        print data
 
-def talk(socket, setence):
-    print 'talk() not implemented'
+def talk(socket, sentence):
+    socket.send(sentence)
+    data = socket.recv(1024)
+
+def hello(socket):
+    word = b'\0x01\0xfe\0x00\0x11\0x14\0x10\0x00\0x02\0x00\0x00\0x00\0x00\0x00\0x00\0x00\0x00\0x00'
+    talk(socket, word)
     
 serverip = "61.155.8.130"
 serverport = 350
 
 loginuser = 'dttest'
 loginpass = 'test'
+#loginpass = 'test2' # wrong pass!
 
 socket = conn(serverip, serverport)
 login(socket, loginuser, loginpass)
 
 setence = b'\x01\x02'
-talk(socket, setence)
+hello(socket, setence)
